ccb928efb92af437cf2e2974ceae33a129460c0d
Wesley Crosslin <136374861+Weskc12@users.noreply.github.com>
2023-10-31 01:06:15 +0000

    added card component

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 
 
-//// Change "Cards to whatever you need to whatver you need to if you are adding yours in"
 
 export const Cards = () => {
   return (
-    //// These are just the basic cards ive added. They are displaying vertically at the moment. I was having issues with it and will circle back to it tonight during mentoring.
-    //// Also have some classes on the styles.css you may want to look at in case it conflicts with anything. one is .container and other is .card
-    //// if those classes arent used at the end of project we can delete. should only be throwing 2 errors for empty rulesets but that is okay for now
-    /// feel free to add files,code or remove anything that you need for your code to work. 
-    //// Be sure to also check the home.jsx and make sure to import react and whatever name you are using for your .jsx file
 
     <div className="row row-cols-1 row-cols-md-3 g-4">
     <div className="col">
